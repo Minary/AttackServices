d827353ee14e21d1c6b2dbb3c0c2b5c6a731625c
Ruben Unteregger <ruben.unteregger@gmail.com>
2019-01-23 21:01:49 +0100

    Add AttackService DnsPoisoning

---
+++ DnsPoisoning/DnsPoisoning.cs
@@ -0,0 +1,170 @@
+﻿namespace Minary.AttackService.Main
+{
+  using MinaryLib.AttackService.Class;
+  using MinaryLib.AttackService.Enum;
+  using MinaryLib.AttackService.Interface;
+  using System;
+  using System.Diagnostics;
+  using System.IO;
+
+
+  public class AS_DnsPoisoning : IAttackService
+  {
+
+    #region MEMBERS
+
+    private const string serviceName = "DnsPoisoning";
+
+    private ServiceStatus serviceStatus;
+    private AttackServiceParameters serviceParams;
+    private Process poisoningEngProc;
+
+    // DnsPoisoning process config
+    private static string targetHostsFile = ".targethosts";
+    private static string dnsPoisoningHostsFile = ".dnshosts";
+
+    private static string DnsPoisoningProcessName = "DnsPoisoning";
+    private static string DnsPoisoningBinaryPath = Path.Combine(serviceName, "DnsPoisoning.exe");
+
+    #endregion
+
+
+    #region PUBLIC
+
+    public AS_DnsPoisoning(AttackServiceParameters serviceParams)
+    {
+      this.serviceParams = serviceParams;
+      this.serviceStatus = ServiceStatus.NotRunning;
+
+      // Register attack service
+      this.serviceParams.AttackServiceHost.Register(this);
+    }
+
+    #endregion
+
+
+    #region PRIVATE
+
+    private void OnServiceExited(object sender, System.EventArgs e)
+    {
+      int exitCode = -99999;
+
+      try
+      {
+        exitCode = this.poisoningEngProc.ExitCode;
+      }
+      catch (Exception)
+      {
+        exitCode = -99999;
+      }
+
+      this.poisoningEngProc.EnableRaisingEvents = false;
+      this.poisoningEngProc.Exited += null;
+      this.serviceParams.AttackServiceHost.OnServiceExited(serviceName, exitCode);
+    }
+
+    #endregion
+
+
+    #region INTERFACE IAttackService implementation
+
+    #region PROPERTIES
+
+    public string ServiceName { get { return serviceName; } set { } }
+
+    public ServiceStatus Status { get { return this.serviceStatus; } set { this.serviceStatus = value; } }
+
+    #endregion
+
+
+    #region PUBLIC
+
+    public ServiceStatus StartService(StartServiceParameters serviceParameters)
+    {
+      var targetHostsRecords = string.Empty;
+      var workingDirectory = Path.Combine(this.serviceParams.AttackServicesWorkingDirFullPath, serviceName);
+      var targetHostsFullPath = Path.Combine(workingDirectory, targetHostsFile);
+      var timeStamp = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+      var processParameters = $"-x {serviceParameters.SelectedIfcId}";
+
+      if (string.IsNullOrEmpty(serviceParameters.SelectedIfcId))
+      {
+        throw new Exception("No interface was declared");
+      }
+
+      if (serviceParameters.TargetList.Count <= 0)
+      {
+        this.serviceStatus = ServiceStatus.NotRunning;
+        this.serviceParams.AttackServiceHost.LogMessage("DnsPoisoning.StartService(): No target system selected");
+        return ServiceStatus.NotRunning;
+      }
+
+      // Write DNS Poisoning spoofing records to list
+      foreach (var tmpTargetMac in serviceParameters.TargetList.Keys)
+      {
+        targetHostsRecords += $"{serviceParameters.TargetList[tmpTargetMac]},{tmpTargetMac}\r\n";
+        this.serviceParams.AttackServiceHost.LogMessage("DnsPoisoning.StartService(): Poisoning targetSystem system: {0}/{1}", tmpTargetMac, serviceParameters.TargetList[tmpTargetMac]);
+      }
+
+      using (var outputFile = new StreamWriter(targetHostsFullPath))
+      {
+        targetHostsRecords = targetHostsRecords.Trim();
+        outputFile.Write(targetHostsRecords);
+      }
+
+      // Start process
+      string dnsPoisoningBinaryFullPath = Path.Combine(this.serviceParams.AttackServicesWorkingDirFullPath, DnsPoisoningBinaryPath);
+
+      this.poisoningEngProc = new Process();
+      this.poisoningEngProc.StartInfo.WorkingDirectory = workingDirectory;
+      this.poisoningEngProc.StartInfo.FileName = dnsPoisoningBinaryFullPath;
+      this.poisoningEngProc.StartInfo.Arguments = processParameters;
+      this.poisoningEngProc.StartInfo.WindowStyle = this.serviceParams.AttackServiceHost.IsDebuggingOn ? ProcessWindowStyle.Normal : ProcessWindowStyle.Hidden;
+      this.poisoningEngProc.StartInfo.CreateNoWindow = this.serviceParams.AttackServiceHost.IsDebuggingOn ? true : false;
+      this.poisoningEngProc.EnableRaisingEvents = true;
+      this.poisoningEngProc.Exited += new EventHandler(this.OnServiceExited);
+      this.serviceStatus = ServiceStatus.Running;
+
+      this.serviceParams.AttackServiceHost.LogMessage("DnsPoisoning.StartService(): CommandLine:{0} {1}", DnsPoisoningBinaryPath, processParameters);
+      this.poisoningEngProc.Start();
+
+      return ServiceStatus.Running;
+    }
+
+
+    public ServiceStatus StopService()
+    {
+      if (this.poisoningEngProc == null)
+      {
+        this.serviceParams.AttackServiceHost.LogMessage("DnsPoisoning.StopService(): Can't stop attack service because it never was started");
+        this.serviceStatus = ServiceStatus.NotRunning;
+        return ServiceStatus.NotRunning;
+      }
+
+      this.poisoningEngProc.EnableRaisingEvents = false;
+      this.poisoningEngProc.Exited += null;
+      this.serviceStatus = ServiceStatus.NotRunning;
+      this.serviceParams.AttackServiceHost.LogMessage("DnsPoisoning.StopService(): EnableRaisingEvents:{0}", this.poisoningEngProc.EnableRaisingEvents);
+
+      try
+      {
+        if (this.poisoningEngProc.HasExited == false)
+        {
+          this.poisoningEngProc.Kill();
+          this.poisoningEngProc = null;
+        }
+      }
+      catch (Exception ex)
+      {
+        this.serviceParams.AttackServiceHost.LogMessage($"DnsPoisoning.StopService(Exception): {ex.Message}");
+      }
+
+      return ServiceStatus.NotRunning;
+    }
+
+    #endregion
+
+    #endregion
+
+  }
+}
